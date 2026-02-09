cbaa9171785fddc515641d279635b567d673d35f
Sokty Heng <sokty@Soktys-MacBook-Pro.local>
2026-02-09 21:00:56 +0700

    change login page

--- lib/auth/auth_page.dart
+++ lib/auth/auth_page.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:skillsync_sp2/pages/login.dart';
-import 'package:skillsync_sp2/pages/register.dart';
 
-class AuthPage extends StatefulWidget {
+class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
-  State<AuthPage> createState() => _AuthPageState();
-}
-
-class _AuthPageState extends State<AuthPage> {
-  bool showLoginPage = true;
-
-  void toggleScreens() {
-    setState(() {
-      showLoginPage = !showLoginPage;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (showLoginPage) {
-      return LoginPage(showRegisterPage: toggleScreens);
-    } else {
-      return RegisterPage(showLoginPage: toggleScreens);
-    }
+    return const LoginPage();
   }
 }
